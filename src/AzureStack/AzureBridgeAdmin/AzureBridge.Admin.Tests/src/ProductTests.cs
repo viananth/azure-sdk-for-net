feeeff00913cc8e643391d345ca81dbc8016a195
Bala Ganapathy <bganapa@microsoft.com>
2018-01-31 10:53:04 -0800

    bridge, update sdk tests

---
+++ src/AzureStack/AzureBridgeAdmin/AzureBridge.Admin.Tests/src/ProductTests.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for
+// license information.
+//
+
+using Microsoft.AzureStack.Management.AzureBridge.Admin;
+using Microsoft.AzureStack.Management.AzureBridge.Admin.Models;
+using System;
+using Xunit;
+
+namespace AzureBridge.Tests
+{
+    public class ProductTests : AzureBridgeTestBase
+    {
+        
+        [Fact]
+        public void TestListPublishedProducts() {
+            RunTest((client) => {
+                var list = client.Activations.List();
+                Common.WriteIPagesToFile(list, client.Activations.ListNext, "TestListPublishedProducts");
+            });
+        }
+
+    }
+}
