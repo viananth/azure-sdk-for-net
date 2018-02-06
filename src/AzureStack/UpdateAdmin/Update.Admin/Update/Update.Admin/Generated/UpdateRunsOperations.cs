e3c5bfe1b7ff28064f77c8ab21792073501c55a7
Bala Ganapathy <bganapa@microsoft.com>
2018-02-06 11:04:52 -0800

    Update SDK

--- src/AzureStack/UpdateAdmin/Update.Admin/Update/Update.Admin/Generated/UpdateRunsOperations.cs
+++ src/AzureStack/UpdateAdmin/Update.Admin/Update/Update.Admin/Generated/UpdateRunsOperations.cs
@@ -120,7 +120,7 @@ namespace Microsoft.AzureStack.Management.Update.Admin
             }
             // Construct URL
             var _baseUrl = Client.BaseUri.AbsoluteUri;
-            var _url = new System.Uri(new System.Uri(_baseUrl + (_baseUrl.EndsWith("/") ? "" : "/")), "subscriptions/{subscriptionId}/resourcegroups/{resourceGroup}/providers/Microsoft.Update.Admin/updateLocations/{updateLocation}/updates{update}/updateRuns").ToString();
+            var _url = new System.Uri(new System.Uri(_baseUrl + (_baseUrl.EndsWith("/") ? "" : "/")), "subscriptions/{subscriptionId}/resourcegroups/{resourceGroup}/providers/Microsoft.Update.Admin/updateLocations/{updateLocation}/updates/{update}/updateRuns").ToString();
             _url = _url.Replace("{subscriptionId}", System.Uri.EscapeDataString(Client.SubscriptionId));
             _url = _url.Replace("{resourceGroup}", System.Uri.EscapeDataString(resourceGroup));
             _url = _url.Replace("{updateLocation}", System.Uri.EscapeDataString(updateLocation));
