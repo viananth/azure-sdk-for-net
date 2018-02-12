183247f326151953ffc4fa5b3a4a13d4155d730f
Bala Ganapathy <bganapa@microsoft.com>
2018-02-11 16:11:13 -0800

    subscriptions admin SDK

---
+++ src/AzureStack/SubscriptionAdmin/Subscription.Admin.Tests/src/DirectoryTenantTests.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for
+// license information.
+//
+
+using Microsoft.AzureStack.Management.Subscriptions.Admin;
+using Microsoft.AzureStack.Management.Subscriptions.Admin.Models;
+using System;
+using Xunit;
+
+namespace Subscriptions.Tests
+{
+    public class DirectoryTenantTests : SubscriptionsTestBase
+    {
+
+        private void ValidateDirectoryTenant(DirectoryTenant item) {
+            // Resource
+            Assert.NotNull(item);
+            Assert.NotNull(item.Id);
+            Assert.NotNull(item.Location);
+            Assert.NotNull(item.Name);
+            Assert.NotNull(item.Type);
+
+            // DirectoryTenant
+            Assert.NotNull(item.TenantId);
+        }
+
+        private void AssertSame(DirectoryTenant expected, DirectoryTenant given) {
+            // Resource
+            Assert.Equal(expected.Id, given.Id);
+            Assert.Equal(expected.Location, given.Location);
+            Assert.Equal(expected.Name, given.Name);
+            Assert.Equal(expected.Type, given.Type);
+
+            // DirectoryTenant
+            Assert.Equal(expected.TenantId, given.TenantId);
+        }
+
+        [Fact]
+        public void TestListDirectoryTenants() {
+            RunTest((client) => {
+                var directoryTenants = client.DirectoryTenants.List("System.local");
+                directoryTenants.ForEach(client.DirectoryTenants.ListNext, ValidateDirectoryTenant);
+            });
+        }
+
+        [Fact]
+        public void TestGetAllDirectoryTenants() {
+            RunTest((client) => {
+                var directoryTenants = client.DirectoryTenants.List("System.local");
+                directoryTenants.ForEach(client.DirectoryTenants.ListNext, (tenant) => {
+                    var result = client.DirectoryTenants.Get("System.local", tenant.Name);
+                    AssertSame(tenant, result);
+                });
+            });
+        }
+
+        [Fact]
+        public void TestGetDirectoryTenant() {
+            RunTest((client) => {
+                var tenant = client.DirectoryTenants.List("System.local").GetFirst();
+                var result = client.DirectoryTenants.Get("System.local", tenant.Name);
+                AssertSame(tenant, result);
+            });
+        }
+
+        [Fact]
+        public void TestCreateUpdateThenDeleteDirectoryTenant() {
+            RunTest((client) => {
+                var tenantDir = new DirectoryTenant(location: "local")
+                {
+                    TenantId = Guid.NewGuid().ToString()
+                };
+                var result = client.DirectoryTenants.CreateOrUpdate("System.local", "testingDirectoryTenant",  tenantDir);
+                tenantDir.TenantId = Guid.NewGuid().ToString();
+                result = client.DirectoryTenants.CreateOrUpdate("System.local", "testingDirectoryTenant", tenantDir);
+                Assert.Equal(tenantDir.TenantId, result.TenantId);
+            });
+        }
+    }
+}
